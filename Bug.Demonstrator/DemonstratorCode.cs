3cb6c383c605de036eee059e675a1071ddf0315a
Frank Robijn <robijn@good-heavens.nl>
2025-07-18 13:06:41 +0200

    Look in DemonstratorCode.cs for the bug.

---
+++ Bug.Demonstrator/DemonstratorCode.cs
@@ -0,0 +1,23 @@
+﻿namespace Bug.Demonstrator
+{
+	public class DemonstratorCode
+	{
+		private readonly int _Value;
+
+		public string Method ()
+		{
+			if (_Value == 0)
+			{
+				var variableToBeRenamed = "The word variableToBeRenamed in a string";
+				return variableToBeRenamed;
+			}
+			else
+			{
+				// Use Ctrl-R R to rename variableToBeRenamed
+				// See how the "variableToBeRenamed: below changes AND IN BOTH STRINGS!
+				var variableToBeRenamed = "The word variableToBeRenamed in another string";
+				return variableToBeRenamed;
+			}
+		}
+	}
+}
